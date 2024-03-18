29cc0a32fcdb89842d918e7f6674b1a7f09180e9
shakhawat1355 <hossain.shakhawat@brainstation-23.com>
2024-03-19 00:21:23 +0600

    update gitIgnore and added class files

---
+++ Class1/Conditions.py
@@ -0,0 +1,10 @@
+a = 33
+b = 33
+
+
+if b > a:
+    print("b is greater tha  n a")
+elif a==b:
+    print("a is equal sdfgs to b")
+else:
+    print("a is greater than b")
---
+++ Class1/DataTypes.py
@@ -0,0 +1,7 @@
+
+name = "someone's Name" # characters
+number = 5 # integer
+x = 20.5 # floats
+NumArray = [1,2,3] # array
+print(number)
+
